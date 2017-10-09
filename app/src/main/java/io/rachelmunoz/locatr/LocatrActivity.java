fa16cea4f19d2efd302570346dc61e754408c933
rachelmunoz <rachelumunoz>
2017-10-08 18:41:35 -0700

    Sets up LocatrFragment for the menu

--- app/src/main/java/io/rachelmunoz/locatr/LocatrActivity.java
+++ app/src/main/java/io/rachelmunoz/locatr/LocatrActivity.java
@@ -1,13 +1,13 @@
 package io.rachelmunoz.locatr;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LocatrActivity extends AppCompatActivity {
+public class LocatrActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_locatr);
+	protected Fragment createFragment() {
+		return LocatrFragment.newInstance();
 	}
 }
